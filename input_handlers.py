bc3a87c4eba457ce9e731bb5a03134dddb0b44b6
Keaton <keaton.wallace@gmail.com>
2019-02-09 12:54:51 -0800

    added existing files to git

---
+++ input_handlers.py
@@ -0,0 +1,33 @@
+import libtcodpy as libtcod
+
+
+def handle_keys(key):
+    key_char = chr(key.c)
+
+    if key.vk == libtcod.KEY_UP or key_char == 'w':
+        return {'move': (0, -1)}
+    elif key.vk == libtcod.KEY_DOWN or key_char == 's':
+        return {'move': (0, 1)}
+    elif key.vk == libtcod.KEY_LEFT or key_char == 'a':
+        return {'move': (-1, 0)}
+    elif key.vk == libtcod.KEY_RIGHT or key_char == 'd':
+        return {'move': (1, 0)}
+    elif key_char == 'q':
+        return {'move': (-1, -1)}
+    elif key_char == 'e':
+        return {'move': (1, -1)}
+    elif key_char == 'z':
+        return {'move': (-1, 1)}
+    elif key_char == 'c':
+        return {'move': (1, 1)}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        # Alt+Enter: toggle full screen
+        return {'fullscreen': True}
+
+    elif key.vk == libtcod.KEY_ESCAPE:
+        # Exit the game
+        return {'exit': True}
+
+    # No key was pressed
+    return {}
